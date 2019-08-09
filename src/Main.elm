94272f022356a68e890630511462d7bd68ba1bb5
Berk Özkütük <berkobrathestabber@gmail.com>
2019-08-10 00:02:35 +0300

    Add togglable triad display

--- src/Main.elm
+++ src/Main.elm
@@ -47,16 +47,23 @@ type Scale
     | Major
 
 
-type alias Model =
+type alias Chord =
     { note : Note
     , accidental : Accidental
     , scale : Scale
     }
 
 
+type alias Model =
+    { chord : Chord
+    , menuVisibility : Bool
+    , triadVisibility : Bool
+    }
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( Model C Natural Major
+    ( Model (Chord C Natural Major) False True
     , Random.generate NewChord randomChord
     )
 
@@ -76,20 +83,32 @@ subscriptions model =
 
 type Msg
     = Change
-    | NewChord Model
+    | NewChord Chord
+    | ToggleMenu
+    | ToggleTriad
     | NoOp
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    case msg of
+    case Debug.log "MESSAGE: " msg of
         Change ->
             ( model
             , Random.generate NewChord randomChord
             )
 
         NewChord newChord ->
-            ( newChord
+            ( { model | chord = newChord }
+            , Cmd.none
+            )
+
+        ToggleMenu ->
+            ( { model | menuVisibility = not model.menuVisibility }
+            , Cmd.none
+            )
+
+        ToggleTriad ->
+            ( { model | triadVisibility = not model.triadVisibility }
             , Cmd.none
             )
 
@@ -107,6 +126,12 @@ keyDecoder =
             case key of
                 " " ->
                     Change
+
+                "s" ->
+                    ToggleMenu
+
+                "t" ->
+                    ToggleTriad
 
                 _ ->
                     NoOp
@@ -129,9 +154,142 @@ scale =
     Random.uniform Major [ Minor ]
 
 
-randomChord : Random.Generator Model
+randomChord : Random.Generator Chord
 randomChord =
-    Random.map3 Model note accidental scale
+    Random.map3 Chord note accidental scale
+
+
+type alias UniqueNote =
+    ( Note, Accidental )
+
+
+half : UniqueNote -> UniqueNote
+half ( n, a ) =
+    case ( n, a ) of
+        ( B, Natural ) ->
+            ( C, Natural )
+
+        ( E, Natural ) ->
+            ( F, Natural )
+
+        ( x, Natural ) ->
+            ( x, Sharp )
+
+        ( C, Flat ) ->
+            ( C, Natural )
+
+        ( F, Flat ) ->
+            ( F, Natural )
+
+        ( x, Flat ) ->
+            ( x, Natural )
+
+        ( A, Sharp ) ->
+            ( B, Natural )
+
+        ( B, Sharp ) ->
+            ( C, Sharp )
+
+        ( C, Sharp ) ->
+            ( D, Natural )
+
+        ( D, Sharp ) ->
+            ( E, Natural )
+
+        ( E, Sharp ) ->
+            ( F, Sharp )
+
+        ( F, Sharp ) ->
+            ( G, Natural )
+
+        ( G, Sharp ) ->
+            ( A, Natural )
+
+
+whole : UniqueNote -> UniqueNote
+whole =
+    half << half
+
+
+iterList : a -> List (a -> a) -> List a
+iterList x l =
+    case l of
+        [] ->
+            [ x ]
+
+        f :: fs ->
+            x :: iterList (f x) fs
+
+
+composeN : Int -> List (a -> a) -> (a -> a)
+composeN n =
+    List.foldr (<<) identity << List.take n
+
+
+majorRecipe : List (UniqueNote -> UniqueNote)
+majorRecipe =
+    [ whole, whole, half, whole, whole, whole, half ]
+
+
+major : UniqueNote -> List UniqueNote
+major x =
+    iterList x majorRecipe
+
+
+majorTriad : UniqueNote -> ( UniqueNote, UniqueNote, UniqueNote )
+majorTriad x =
+    let
+        fFirst =
+            identity
+
+        fThird =
+            composeN 2 majorRecipe
+
+        fFifth =
+            composeN 4 majorRecipe
+    in
+    ( fFirst x
+    , fThird x
+    , fFifth x
+    )
+
+
+minorRecipe : List (UniqueNote -> UniqueNote)
+minorRecipe =
+    [ whole, half, whole, whole, half, whole, whole ]
+
+
+minor : UniqueNote -> List UniqueNote
+minor x =
+    iterList x minorRecipe
+
+
+minorTriad : UniqueNote -> ( UniqueNote, UniqueNote, UniqueNote )
+minorTriad x =
+    let
+        fFirst =
+            identity
+
+        fThird =
+            composeN 2 minorRecipe
+
+        fFifth =
+            composeN 4 minorRecipe
+    in
+    ( fFirst x
+    , fThird x
+    , fFifth x
+    )
+
+
+triad : Scale -> UniqueNote -> ( UniqueNote, UniqueNote, UniqueNote )
+triad s =
+    case s of
+        Major ->
+            majorTriad
+
+        Minor ->
+            minorTriad
 
 
 
@@ -147,59 +305,88 @@ boxTextStyle size =
     ]
 
 
+noteStr : Note -> String
+noteStr x =
+    case x of
+        C ->
+            "C"
+
+        D ->
+            "D"
+
+        E ->
+            "E"
+
+        F ->
+            "F"
+
+        G ->
+            "G"
+
+        A ->
+            "A"
+
+        B ->
+            "B"
+
+
+accStr : Accidental -> String
+accStr x =
+    case x of
+        Sharp ->
+            "♯"
+
+        Flat ->
+            "♭"
+
+        Natural ->
+            ""
+
+
+scaleStr : Scale -> String
+scaleStr x =
+    case x of
+        Major ->
+            ""
+
+        Minor ->
+            "m"
+
+
+uniqueStr : UniqueNote -> String
+uniqueStr ( n, a ) =
+    noteStr n ++ accStr a
+
+
 viewChord : Model -> El.Element Msg
 viewChord model =
-    let
-        noteStr : Note -> String
-        noteStr x =
-            case x of
-                C ->
-                    "C"
-
-                D ->
-                    "D"
-
-                E ->
-                    "E"
-
-                F ->
-                    "F"
-
-                G ->
-                    "G"
-
-                A ->
-                    "A"
-
-                B ->
-                    "B"
-
-        accStr : Accidental -> String
-        accStr x =
-            case x of
-                Sharp ->
-                    "♯"
-
-                Flat ->
-                    "♭"
-
-                Natural ->
-                    ""
-
-        scaleStr : Scale -> String
-        scaleStr x =
-            case x of
-                Major ->
-                    ""
-
-                Minor ->
-                    "m"
-    in
-    noteStr model.note
-        ++ accStr model.accidental
-        ++ scaleStr model.scale
+    noteStr model.chord.note
+        ++ accStr model.chord.accidental
+        ++ scaleStr model.chord.scale
         |> El.text
         |> El.el (boxTextStyle 80)
+
+
+viewTriad : Model -> El.Element Msg
+viewTriad model =
+    case model.triadVisibility of
+        True ->
+            let
+                triadStr ( x, y, z ) =
+                    uniqueStr x
+                        ++ " - "
+                        ++ uniqueStr y
+                        ++ " - "
+                        ++ uniqueStr z
+            in
+            triad model.chord.scale ( model.chord.note, model.chord.accidental )
+                |> triadStr
+                |> El.text
+                |> El.el (boxTextStyle 40)
+                |> viewColumnBox 1
+
+        False ->
+            El.none
 
 
 colors =
@@ -209,27 +396,44 @@ colors =
     }
 
 
+boxStyle : List (El.Attribute Msg)
+boxStyle =
+    [ Bg.color colors.box
+    , El.width El.fill
+    , El.height El.fill
+    , Border.rounded 50
+    ]
+
+
+viewRowBox : Int -> El.Element Msg -> El.Element Msg
+viewRowBox portion =
+    El.el <|
+        boxStyle
+            ++ [ El.width <| El.fillPortion portion ]
+
+
+viewColumnBox : Int -> El.Element Msg -> El.Element Msg
+viewColumnBox portion =
+    El.el <|
+        boxStyle
+            ++ [ El.height <| El.fillPortion portion ]
+
+
+viewMenu : Model -> El.Element Msg
+viewMenu model =
+    case model.menuVisibility of
+        True ->
+            "Settings"
+                |> El.text
+                |> El.el (boxTextStyle 40)
+                |> viewRowBox 1
+
+        False ->
+            El.none
+
+
 view : Model -> Html Msg
 view model =
-    let
-        r1 =
-            El.el
-                [ Bg.color colors.box
-                , El.width El.fill
-                , El.height <| El.fillPortion 8
-                , Border.rounded 50
-                ]
-                (viewChord model)
-
-        r2 =
-            El.el
-                [ Bg.color colors.box
-                , El.width El.fill
-                , El.height <| El.fillPortion 1
-                , Border.rounded 50
-                ]
-                (El.el (boxTextStyle 40) <| El.text "bok")
-    in
     El.layout [] <|
         El.el
             [ Bg.color colors.bg
@@ -238,12 +442,20 @@ view model =
             , El.width El.fill
             ]
         <|
-            El.column
-                [ El.centerX
-                , El.centerY
-                , El.width El.fill
+            El.row
+                [ El.width El.fill
                 , El.height El.fill
                 , El.spacing 50
                 ]
-                [ r1
+                [ El.column
+                    [ El.centerX
+                    , El.centerY
+                    , El.width <| El.fillPortion 4
+                    , El.height El.fill
+                    , El.spacing 50
+                    ]
+                    [ viewColumnBox 8 (viewChord model)
+                    , viewTriad model
+                    ]
+                , viewMenu model
                 ]
